182d9ac5bc9d04d6f763844a79bd9b593703e01c
aditya3434 <aditya390402@gmail.com>
2022-09-09 13:43:21 +0530

    First commit

---
+++ SingleAgent/my_env.py
@@ -0,0 +1,128 @@
+import gym
+from gym.spaces import Box
+import numpy as np
+import pygame
+
+FPS = 100
+GAP = 100
+COLLISION_DIST = 55
+OFFROAD_DIST = 35
+
+class Vehicle():
+
+    def __init__(self, x_initial=0, y_initial=0, v_initial=0, angle_initial=0):
+        self.x = x_initial
+        self.y = y_initial
+        self.v = v_initial
+        self.angle = angle_initial
+        self.acc = 0
+        self.color = (255, 0, 0)
+
+    def take_action(self, action, dt):
+
+        if action is None:
+            return
+
+        self.acc = action[0]
+        self.angle += 5*action[1]*dt
+        self.v += 5*self.acc*dt
+        self.x += 5*self.v*np.cos(self.angle*np.pi/180)*dt
+        self.y += 5*self.v*np.sin(self.angle*np.pi/180)*dt
+
+def dist_euclid(v1, v2):
+    return np.sqrt((v1.x-v2.x)**2+(v1.y-v2.y)**2)
+
+class myEnv(gym.Env):
+
+    def __init__(self):
+        self.action_space = Box(-1, 1, shape=(1,), dtype=np.float32)
+        self.observation_space = Box(-np.inf, np.inf, shape=(3,), dtype=np.float32)
+
+        self.back_car = Vehicle(x_initial=0, y_initial=400)
+        self.front_car = Vehicle(x_initial=2*GAP, y_initial=400)
+
+        self.ego_car = Vehicle(x_initial=GAP, y_initial=400)
+
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.dt = self.clock.tick(FPS)/1000
+        self.screen = pygame.Surface((800, 600))
+
+    def step(self, action):
+
+        reward = 0
+        done = False
+
+        action[0] = np.clip(action[0], 0, 1)
+
+        if (action[0] <= 0 and self.ego_car.v <= 0):
+            action[0] *= -1
+
+        self.ego_car.take_action([action[0], 0], self.dt)
+        self.back_car.take_action([0.5, 0], self.dt)
+        self.front_car.take_action([0.5, 0], self.dt)
+
+        if (self.ego_car.x >= 950):
+            reward += 1000
+            done = True
+        
+        if abs(self.ego_car.y-400) > OFFROAD_DIST:
+            reward -= 100
+            done = True
+
+        back_dist = dist_euclid(self.ego_car, self.back_car)
+        front_dist = dist_euclid(self.ego_car, self.front_car)
+
+        if (back_dist < COLLISION_DIST or front_dist < COLLISION_DIST):
+            reward -= 100
+            done = True
+
+        dist_error = abs(front_dist-back_dist)
+
+        reward += (self.ego_car.x//200)*10
+
+        if (dist_error <= 5):
+            reward += 1
+
+        obs = self.feature_scaling(np.hstack((self.ego_car.x, back_dist, front_dist)))
+
+        return np.array(obs, dtype=np.float32), reward, done, None
+
+    def reset(self):
+        self.back_car = Vehicle(x_initial=0, y_initial=400)
+        self.front_car = Vehicle(x_initial=2*GAP, y_initial=400)
+        self.ego_car = Vehicle(x_initial=GAP, y_initial=400)
+
+        self.state_max = np.hstack((1000, 2*GAP, 2*GAP))
+        self.state_min = np.hstack((0, 0, 0))
+
+        return np.array([0, 0.5, 0.5], dtype=np.float32)
+
+    def draw(self):
+
+        pygame.draw.rect(self.screen, (125, 125, 125), [0, 400, 1000, 75])
+        finish = pygame.image.load('../finish.jpg')
+        finish = pygame.transform.rotate(finish, 90)
+        finish = pygame.transform.scale(finish, (30, 75))
+        self.screen.blit(finish, (950, 400))
+
+        sprite = pygame.image.load('../car.png')
+        sprite = pygame.transform.rotate(sprite, -90)
+        sprite = pygame.transform.scale(sprite, (80, 80))
+
+        self.screen.blit(sprite, (self.back_car.x, self.back_car.y))
+        self.screen.blit(sprite, (self.front_car.x, self.front_car.y))
+
+        sprite = pygame.transform.rotate(sprite, (-1)*self.ego_car.angle)
+
+        self.screen.blit(sprite, (self.ego_car.x, self.ego_car.y))
+        
+
+    def render(self):
+        self.screen = pygame.display.set_mode((1000, 800))
+        self.screen.fill((255, 255, 255))
+        self.draw()
+        pygame.display.flip()
+
+    def feature_scaling(self, state):
+        return (state - self.state_min) / (self.state_max - self.state_min)
---
+++ convoy.py
@@ -0,0 +1,192 @@
+from gym.spaces import Box
+import numpy as np
+import functools
+from pettingzoo import AECEnv
+from pettingzoo.utils import agent_selector
+import pygame
+
+FPS = 100
+GAP = 90
+COLLISION_DIST = 65
+OFFROAD_DIST = 35
+
+class Vehicle():
+
+    def __init__(self, x_initial=0, y_initial=0, v_initial=0, angle_initial=0):
+        self.x = x_initial
+        self.y = y_initial
+        self.v = v_initial
+        self.angle = angle_initial
+        self.acc = 0
+        self.color = (255, 0, 0)
+
+    def take_action(self, action, dt):
+
+        if action is None:
+            return
+
+        self.acc = action[0]
+        self.angle += 10*action[1]*dt
+        self.v += 10*self.acc*dt
+        self.x += 10*self.v*np.cos(self.angle*np.pi/180)*dt
+        self.y += 10*self.v*np.sin(self.angle*np.pi/180)*dt
+
+def dist_euclid(v1, v2):
+    return np.sqrt((v1.x-v2.x)**2+(v1.y-v2.y)**2)
+        
+
+def env(n_vehicles=3):
+    env = raw_env(n_vehicles=n_vehicles)
+    return env
+
+
+class raw_env(AECEnv):
+
+    metadata = {"render_modes": ["human"], "name": "rps_v2"}
+
+    def __init__(self, n_vehicles=3):
+        
+        self.possible_agents = ["vehicle_" + str(r) for r in range(n_vehicles)]
+        self.vehicles = [Vehicle(x_initial=r*GAP, y_initial=400) for r in range(n_vehicles)]
+        self.agent_name_mapping = dict(
+            zip(self.possible_agents, list(range(len(self.possible_agents))))
+        )
+
+        self._action_spaces = {agent: Box(0, 1.0, (1, 2)) for agent in self.possible_agents}
+        self._observation_spaces = {
+            agent: Box(-1000, 1000, (1, 3)) for agent in self.possible_agents
+        }
+
+        self.done = False
+
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.dt = self.clock.tick(FPS)/1000
+        self.screen = pygame.Surface((800, 600))
+
+    @functools.lru_cache(maxsize=None)
+    def observation_space(self, agent):
+        return Box(-1000, 1000, (1, 3))
+
+    @functools.lru_cache(maxsize=None)
+    def action_space(self, agent):
+        return Box(0, 1.0, (1,))
+
+    def draw(self):
+
+        pygame.font.init()
+        my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
+        pygame.draw.rect(self.screen, (125, 125, 125), [0, 400, 1000, 75])
+        finish = pygame.image.load('finish.jpg')
+        finish = pygame.transform.rotate(finish, 90)
+        finish = pygame.transform.scale(finish, (30, 75))
+        self.screen.blit(finish, (950, 400))
+
+        for agent in self.agents:
+            vehicle = self.vehicles[self.agent_name_mapping[agent]]
+            if self.dones[agent]:
+                continue
+            sprite = pygame.image.load('car.png')
+            sprite = pygame.transform.rotate(sprite, -90)
+            sprite = pygame.transform.scale(sprite, (80, 80))
+
+            sprite = pygame.transform.rotate(sprite, (-1)*vehicle.angle)
+            self.screen.blit(sprite, (vehicle.x, vehicle.y))
+
+        for index, agent in enumerate(self.agents):
+            vehicle = self.vehicles[self.agent_name_mapping[agent]]
+            text_surface = my_font.render(f'{agent} : [x: {round(vehicle.x, 3)}, y: {round(vehicle.y, 3)}, vel: {round(vehicle.v, 3)}, angle: {round(vehicle.angle, 3)}]', False, (0, 0, 0))
+            self.screen.blit(text_surface, (0,index*30))
+
+    def render(self, mode="human"):
+        self.screen = pygame.display.set_mode((1000, 800))
+        self.screen.fill((255, 255, 255))
+        self.draw()
+        pygame.display.flip()
+
+    def observe(self, agent):
+        return np.array(self.observations[agent])
+
+    def close(self):
+        pygame.event.pump()
+        pygame.display.quit()
+
+    def reset(self, seed=None, options=None):
+        
+        self.agents = self.possible_agents[:]
+        self.rewards = {agent: 0 for agent in self.agents}
+        self._cumulative_rewards = {agent: 0 for agent in self.agents}
+        self.dones = {agent: False for agent in self.agents}
+        self.infos = {agent: {} for agent in self.agents}
+        self.state = {agent: None for agent in self.agents}
+        self.observations = {agent: None for agent in self.agents}
+        self.num_moves = 0
+        """
+        Our agent_selector utility allows easy cyclic stepping through the agents list.
+        """
+        self._agent_selector = agent_selector(self.agents)
+        self.agent_selection = self._agent_selector.next()
+
+        self.vehicles = [Vehicle(x_initial=r*GAP, y_initial=400) for r in range(len(self.vehicles))]
+        self.done = False
+
+        return np.array([0])
+
+    def step(self, action):
+        
+        # if self.dones[self.agent_selection]:
+        #     # handles stepping an agent which is already done
+        #     # accepts a None action for the one agent, and moves the agent_selection to
+        #     # the next done agent,  or if there are no more done agents, to the next live agent
+        #     return self._was_done_step(action)
+
+        if all(x for x in self.dones.values()):
+            self.done=True
+            return
+
+        agent = self.agent_selection
+        vehicle = self.vehicles[self.agent_name_mapping[agent]]
+
+        self._cumulative_rewards[agent] = 0
+
+        self.state[self.agent_selection] = action
+
+        vehicle.take_action(action, self.dt)
+
+        if self._agent_selector.is_last():
+            
+            for agent in self.agents:
+                vehicle = self.vehicles[self.agent_name_mapping[agent]]
+                dist = 1000-vehicle.x
+                self.rewards[agent] = (100/dist)
+                if (vehicle.x >= 950):
+                    self.dones[agent] = True
+                    vehicle.color = (0, 0, 255)
+                    self.rewards[agent] += 1000
+
+                index = self.agent_name_mapping[agent]
+
+                if abs(vehicle.y-400) > OFFROAD_DIST:
+                    self.rewards[agent] -= 1000
+                    self.done = True
+                    print("Vehicle offroad!")
+                
+                if index == 0 or index == len(self.agents)-1 or self.dones[agent]:
+                    continue
+
+                front = dist_euclid(vehicle, self.vehicles[index+1])
+                back = dist_euclid(vehicle, self.vehicles[index-1])
+
+                if (front < COLLISION_DIST and self.dones[self.agents[index+1]] != True) or (back < COLLISION_DIST and self.dones[self.agents[index-1]] != True):
+                    self.rewards[agent] -= 1000
+                    self.done = True
+                    print("Collision detected!")
+
+            for i in self.agents:
+                self.observations[i] = self.vehicles[self.agent_name_mapping[i]].x
+        else:
+            self._clear_rewards()
+
+        self.agent_selection = self._agent_selector.next()
+        self._accumulate_rewards()
